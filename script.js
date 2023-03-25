937cfe7e8221b82cd8fb5bfc283d415b0eb8666c
William Hill <williamhill@Williams-MacBook-Pro.local>
2023-03-25 15:13:48 -0400

    Building calculator layout and worked on some functions

---
+++ script.js
@@ -0,0 +1,29 @@
+// Variables
+let firstOperand = null;
+let secondOperand = null;
+let currentOpeartor = null;
+
+// add
+function add(a, b) {
+    return a + b
+}
+
+// subtract
+function subtract(a, b) {
+    return a - b
+}
+
+// divide 
+function divide(a, b) {
+    return a / b
+}
+
+// multiply 
+function multiply(a, b) {
+    return a * b 
+}
+
+//operations
+// function operate(num1, operator, num2) {
+    
+// }
