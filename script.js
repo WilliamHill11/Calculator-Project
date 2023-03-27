93ae9fa3d99d22d0d4a50a017b3f568f25192d03
william hill <williamhill0831@Hotmail.com>
2023-03-27 15:11:44 -0400

    Making tiny progress on functionality

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let firstOperand = null;
 let secondOperand = null;
 let currentOperator = null;
 
+function operations (operator) {
+    if (operator === '+') {
+        add(firstOperand, secondOperand)
+
+    }
+}
 // add
 function add(a, b) {
     return a + b
@@ -23,21 +29,30 @@ function multiply(a, b) {
     return a * b 
 }
 
-// operations
+// operate
 function operate(num1, operator, num2) {
-    return num1 + operator + num2
+    num1 + operator + num2
+    if (operator) {
+        return operations(operator)
+    }
 }
 
 // display value 
-const displayCurrentOperand = document.querySelector('.currentOperand');
-let initialValue = document.createElement('p');
-initialValue.textContent = '0';
-displayCurrentOperand.append(initialValue);
+// const displayCurrentOperand = document.querySelector('.display');
+let currentOperand = document.querySelector('.currentOperand');
+currentOperand.textContent = '';
+
+let perviousOperand = document.querySelector('.perviousOperand');
 
 
 const btn = document.querySelectorAll('.button');
 const ac = document.querySelector('#reset');
 const del = document.querySelector('#del');
+const equals = document.querySelector('#equals');
+
+equals.addEventListener('click', () => {
+    console.log('hi')
+})
 
 // del.addEventListener('click', () => {
 //     initialValue.pop()
@@ -51,34 +66,35 @@ ac.addEventListener('click', () => {
 
 
 btn.forEach (button => {
-    if (button.dataset.type === 'operator') {
-        operate()
-        return
-    }
-    else if (button.dataset.type === 'number') {
-        let number = button;
-        number.addEventListener('click', (e) => {
+    if (button.dataset.type === 'number') {
+        let digits = button;
+        digits.addEventListener('click', (e) => {
             let click = e.target;
-            let numberClicked = click.value;
-            firstOperand = numberClicked;
-            initialValue.append(numberClicked)
+            let numberClicked = parseInt(click.value);
+            currentOperand.append(numberClicked)
+            firstOperand = currentOperand.innerHTML;
+            console.log(parseInt(firstOperand + 5));
             console.log(firstOperand, 'firstOperand')
         })
     }
 })
 
-function operate() {
-    btn.forEach (operatorButton => {
-        if (operatorButton.dataset.type === 'operator') {
-            let operator = operatorButton;
-            operator.addEventListener('click', (e) => {
-                let click = e.target;
-                let operatorClicked = click.value
-                initialValue.append(operatorClicked)
-                // if (operatorClicked === '+') {
-                //     add()
-                // }
-            })
-        }
-    })
-}
+
+btn.forEach (operatorButton => {
+    if (operatorButton.dataset.type === 'operator') {
+        let operator = operatorButton;
+        operator.addEventListener('click', (e) => {
+            let click = e.target;
+            let operatorClicked = click.value
+            perviousOperand.append(firstOperand+operatorClicked)
+            return operate(firstOperand, operatorClicked, num2=null)
+            // if (operatorClicked === '+') {
+            //     add()
+            // }
+        })
+    }
+})
+
+//numbers entered should be stored in first operand
+//then when operator is clicked it should take the first value and the next
+//value and evaluate. Then if they should be able to keep adding
