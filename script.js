748235f0650cd698b1396188e66c38370e219c9e
william hill <williamhill0831@Hotmail.com>
2023-03-27 01:24:39 -0400

    added functionality when clicking on buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Variables
 let firstOperand = null;
 let secondOperand = null;
-let currentOpeartor = null;
+let currentOperator = null;
 
 // add
 function add(a, b) {
@@ -33,3 +33,39 @@ const displayCurrentOperand = document.querySelector('.currentOperand');
 let initialValue = document.createElement('p');
 initialValue.textContent = '0';
 displayCurrentOperand.append(initialValue);
+
+
+const btn = document.querySelectorAll('.button');
+const ac = document.querySelector('#reset');
+const del = document.querySelector('#del');
+
+del.addEventListener('click', () => {
+    initialValue.pop()
+})
+
+
+ac.addEventListener('click', () => {
+    location.reload()
+})
+
+
+
+btn.forEach (button => {
+    if (button.dataset.type === 'number') {
+        let number = button;
+        number.addEventListener('click', (e) => {
+            let click = e.target;
+            let numberClicked = click.value;
+            initialValue.append(numberClicked)
+        } )
+        console.log(button)
+    }
+    else if (button.dataset.type === 'operator') {
+        let operator = button;
+        operator.addEventListener('click', (e) => {
+            let click = e.target;
+            let operatorClicked = click.value
+            initialValue.append(operatorClicked)
+        })
+    }
+})
