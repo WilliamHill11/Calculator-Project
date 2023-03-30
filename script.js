2f97aff3fe47c8bf228c8db9c9feebb62f375067
william hill <williamhill0831@Hotmail.com>
2023-03-29 23:51:13 -0400

    working on button functions

--- script.js
+++ script.js
@@ -2,12 +2,11 @@
 let firstOperand = '';
 let secondOperand = null;
 let currentOperator = null; 
-let currentOperand = document.querySelector('.currentOperand');
+let currentOperandScreen = document.querySelector('.currentOperand');
 let display = document.querySelector('.display');
 let perviousOperand = document.querySelector('.perviousOperand');
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
-currentOperand.textContent = '';
 const btn = document.querySelectorAll('.button');
 const ac = document.querySelector('#reset');
 const del = document.querySelector('#del');
@@ -33,10 +32,25 @@ function multiply(a, b) {
     return a * b 
 }
 
-numberButtons.forEach((button) => {
-    button.addEventListener('click', addNumberToScreen)
-})
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => addNumberToScreen(button.textContent))
+)
 
+operatorButtons.forEach((operator) => 
+    operator.addEventListener('click', () => setOperator(operator.textContent))
+)
+    
+function addNumberToScreen(number) {
+    return currentOperandScreen.textContent += number;
+}
+
+function setOperator(operator) {
+    return currentOperandScreen.textContent += operator;
+}
+
+equals.addEventListener('click', () => {
+    operate(firstOperand, operatorClicked, secondOperand)
+})
 
 // operate
 function operate(num1, operator, num2) {
@@ -65,50 +79,50 @@ ac.addEventListener('click', () => {
     location.reload()
 })
 
-btn.forEach (button => {
-    if (button.dataset.type === 'number') {
-        let digits = button;
-        digits.addEventListener('click', (e) => {
-            let click = e.target;
-            let numberClicked = parseInt(click.value);
-            currentOperand.append(numberClicked)
-            // firstOperand =  parseInt(currentOperand.innerHTML);
-            if (firstOperand !== perviousOperand)  {
-                firstOperand =  parseInt(currentOperand.innerHTML);
-            } else {
-                secondOperand = parseInt(currentOperand.innerHTML);
-            }
+// btn.forEach (button => {
+//     if (button.dataset.type === 'number') {
+//         let digits = button;
+//         digits.addEventListener('click', (e) => {
+//             let click = e.target;
+//             let numberClicked = parseInt(click.value);
+//             currentOperand.append(numberClicked)
+//             // firstOperand =  parseInt(currentOperand.innerHTML);
+//             if (firstOperand !== perviousOperand)  {
+//                 firstOperand =  parseInt(currentOperand.innerHTML);
+//             } else {
+//                 secondOperand = parseInt(currentOperand.innerHTML);
+//             }
 
 
-            // console.log(parseInt(firstOperand + 5));
-            // console.log(firstOperand, 'firstOperand')
-        })
-    }
-})
+//             // console.log(parseInt(firstOperand + 5));
+//             // console.log(firstOperand, 'firstOperand')
+//         })
+//     }
+// })
 
 
-btn.forEach (operatorButton => {
-    if (operatorButton.dataset.type === 'operator') {
-        let operator = operatorButton;
-        operator.addEventListener('click', (e) => {
-            let click = e.target;
-            let operatorClicked = click.value
-            perviousOperand.append(firstOperand + operatorClicked)
-            perviousOperand = firstOperand
-            currentOperand.textContent = secondOperand;
-            secondOperand = currentOperand.innerHTML;
-            console.log(secondOperand, 'hi')
-            equals.addEventListener('click', () => {
-                    console.log(firstOperand, 'first')
-                    console.log(secondOperand, 'second')
-                    console.log(operatorClicked, 'operator')
-                    console.log(perviousOperand, 'pervious')
-                    operate(firstOperand, operatorClicked, secondOperand)
-            })
-        })
-    }
+// btn.forEach (operatorButton => {
+//     if (operatorButton.dataset.type === 'operator') {
+//         let operator = operatorButton;
+//         operator.addEventListener('click', (e) => {
+//             let click = e.target;
+//             let operatorClicked = click.value
+//             perviousOperand.append(firstOperand + operatorClicked)
+//             perviousOperand = firstOperand
+//             currentOperand.textContent = secondOperand;
+//             secondOperand = currentOperand.innerHTML;
+//             console.log(secondOperand, 'hi')
+//             equals.addEventListener('click', () => {
+//                     console.log(firstOperand, 'first')
+//                     console.log(secondOperand, 'second')
+//                     console.log(operatorClicked, 'operator')
+//                     console.log(perviousOperand, 'pervious')
+//                     operate(firstOperand, operatorClicked, secondOperand)
+//             })
+//         })
+//     }
     
-})
+// })
 
 
 
